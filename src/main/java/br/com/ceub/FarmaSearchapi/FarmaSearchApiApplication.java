029d14e1694a7398900e829a7386dcc67d9a550c
Marco Couto <couto.marcoantonio@gmail.com>
2022-03-21 10:03:57 -0300

    Inicialização do SprintBoot com Java 17 e Gradle

---
+++ src/main/java/br/com/ceub/FarmaSearchapi/FarmaSearchApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ceub.FarmaSearchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaSearchApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ceub/FarmaSearchapi/FarmaSearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ceub.FarmaSearchapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaSearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
